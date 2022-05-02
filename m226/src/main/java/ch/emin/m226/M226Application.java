ba113c10a7fce99b68125f974cf68c7d05c76ba2
vagrant Super Idol <vagrant@m226Bvm>
2022-05-02 15:14:29 +0200

    V 1.0

---
+++ m226/src/main/java/ch/emin/m226/M226Application.java
@@ -0,0 +1,13 @@
+package ch.emin.m226;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M226Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M226Application.class, args);
+	}
+
+}
---
+++ m226/src/test/java/ch/emin/m226/M226ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.emin.m226;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M226ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
